8cf794cafcb2d9659e5338813005caf96bb8103a
Quena <quenallatafranco@gmail.com>
2024-01-09 17:21:49 -0300

    rename

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const archivos = require("./archivos.js");
+const archivos = require("./archivosRouter.js");
 
 router.use("/archivos", archivos);
 
